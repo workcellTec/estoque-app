457a20dbbeb3050d2cbf9c370f7104c000cd66d2
Projetcts my <brendon.expert@gmail.com>
2026-06-03 11:24:46 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // sw.js — Central Workcell
 // Arquivo deve ficar na raiz do repositório (mesma pasta do index.html)
 
-const CACHE = 'ctw-182';
+const CACHE = 'ctw-183';
 
 self.addEventListener('install', e => {
     self.skipWaiting();
